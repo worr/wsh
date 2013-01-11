dc44b67480cc417eee5512810490878dcb566358
Will Orr <will@worrbase.com>
2013-01-11 05:13:17 -0500

    Major changes to command snippets
    - now more intelligently form sudo command
    - works with older Glibs with no functional change
    - waits for child process and collects output

--- server/src/cmd.c
+++ server/src/cmd.c
@@ -1,50 +1,82 @@
 #include "cmd.h"
 
-#include <cmd-messages.pb-c.h>
-
 #include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-gint run_cmd(struct cmd_res* res, struct cmd_req* req) {
-	if (req->sudo) {
-		gchar* new_cmd_string = g_malloc0(strlen(req->cmd_string) +
-		strlen("/usr/bin/sudo") + 2);
-		g_strlcpy(new_cmd_string, "/usr/bin/sudo ", strlen("/usr/bin/sudo") +
-		2);
-		g_strlcat(new_cmd_string, req->cmd_string, strlen(req->cmd_string) +
-		strlen("/usr/bin/sudo") + 2);
+// retval should be g_free'd
+static gchar* construct_sudo_cmd(const struct cmd_req* req) {
+	// If not sudo, don't actually construct the command
+	if (! req->sudo)
+		return g_strdup(req->cmd_string);
 
-		req->cmd_string = new_cmd_string;
+	// Construct cmd to escalate privs assuming use of sudo
+	// Should probably get away from relying explicitly on sudo, but it's good
+	// enough for now
+
+	// Thanks to glib this is basically a single function call
+	return g_strconcat(SUDO_CMD, req->username, req->cmd_string, NULL);
+}
+
+gint run_cmd(struct cmd_res* res, struct cmd_req* req) {
+	gchar* cmd = construct_sudo_cmd(req);
+	gchar* old_path;
+	res->err = g_new0(GError, 1);
+	pid_t pid;
+	gint stat;
+	gint flags = G_SPAWN_DO_NOT_REAP_CHILD;
+
+// sorry Russ...
+#if GLIB_CHECK_VERSION ( 2, 34, 0 )
+	flags |= G_SPAWN_SEARCH_PATH_FROM_ENVP;
+#else
+	flags |= G_SPAWN_SEARCH_PATH;
+#endif
+
+	if (glib_check_version(2, 34, 0)) {
+		old_path = g_strdup(g_getenv("PATH"));
+		g_setenv(g_environ_getenv(req->env, "PATH"), "PATH", TRUE);
 	}
 
 	g_spawn_async_with_pipes(
-		NULL, 
-		g_strsplit_set(req->cmd_string, " \t\n\r", MAX_CMD_ARGS),
-		req->env,
-		G_SPAWN_CHILD_INHERITS_STDIN | G_SPAWN_SEARCH_PATH_FROM_ENVP,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		&res->out_fd,
-		&res->err_fd,
-		&res->err);
+		req->cwd,  // working dir
+		g_strsplit_set(cmd, " \t\n\r", MAX_CMD_ARGS), // argv
+		req->env, // env
+		flags, // flags
+		NULL, // child setup
+		NULL, // user_data
+		&pid, // child_pid
+		NULL, // stdin
+		&res->out_fd, // stdout
+		&res->err_fd, // stderr
+		&res->err); // Gerror
 
-	char* buf = g_malloc0(255);
-	read(res->out_fd, buf, 255);
-	printf("%s\n", buf);
-	free(buf);
+	// wait()s to get status of called function, so we can report it back to
+	// the user and so we don't blow away path or anything
+	if (waitpid(pid, &stat, 0) != -1) {
+		res->exit_status = WEXITSTATUS(stat);
+	} else {
+		return EXIT_FAILURE;
+	}
 
-	buf = g_malloc0(255);
-	read(res->err_fd, buf, 255);
-	printf("%s\n", buf);
-	free(buf);
+	if (glib_check_version(2, 34, 0)) {
+		g_setenv("PATH", old_path, TRUE);
+		g_free(old_path);
+	}
 
-	if (res->err != NULL)
+	gchar* out = g_malloc0(1024);
+	read(res->out_fd, out, 1024);
+	printf("%s", out);
+	printf("%d\n", res->exit_status);
+	if (res->err != NULL) {
+		printf("%d\n", res->err->code);
 		printf("%s\n", res->err->message);
+	}
+
+	g_free(cmd);
+	g_free(res->err);
 
 	return EXIT_SUCCESS;
 }
--- server/src/main.c
+++ server/src/main.c
@@ -9,8 +9,8 @@ int main(int argc, char** argv, char** env) {
 	struct cmd_req req;
 	req.env = env;
 	req.in_fd = -1;
-	req.cmd_string = "whoami";
-	req.sudo = 1;
+	req.cmd_string = "./test.sh 0";
+	req.sudo = 0;
 
 	struct cmd_res res;
 	run_cmd(&res, &req);

a506e67e704b767eb039622fe4c4e599082c606e
Will Orr <will@worrbase.com>
2013-02-20 22:31:05 -0500

    Fixed tests

--- server/test/test_run_cmd.c
+++ server/test/test_run_cmd.c
@@ -47,13 +47,13 @@ static void test_run_exit_code(struct test_wsh_run_cmd_data* fixture, gconstpoin
 	wsh_cmd_req_t* req = fixture->req;
 	wsh_cmd_res_t* res = fixture->res;
 
-	req->cmd_string = "/bin/ls";
+	req->cmd_string = "/bin/sh -c 'exit 0'";
 	gint ret = wsh_run_cmd(res, req);
 	g_assert(ret == 0);
 	g_assert_no_error(res->err);
 	g_assert(res->exit_status == 0);
 
-	req->cmd_string = "/bin/ls /asdfasdfasf";
+	req->cmd_string = "/bin/sh -c 'exit 1'";
 	g_assert(wsh_run_cmd(res, req) == 0);
 	g_assert_no_error(res->err);
 	g_assert(res->exit_status == 1);

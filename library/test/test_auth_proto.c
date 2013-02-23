43f1d7f9408e6fcacaf23eeea3cb7186cfef1e47
Will Orr <will@worrbase.com>
2013-02-23 05:41:13 -0500

    Added test for corrupted AuthInfo messages

--- library/test/test_auth_proto.c
+++ library/test/test_auth_proto.c
@@ -1,11 +1,17 @@
 #include <glib.h>
+#include <stdlib.h>
 
 #include "auth.pb-c.h"
 
-static const guint8 ai_encoded[12] = { 0x0a, 0x04, 0x77, 0x69, 0x6c, 0x6c, 0x12, 0x04, 0x74, 0x65, 0x73, 0x74 };
 static const gsize ai_encoded_len = 12;
+static const guint8 ai_encoded[12] 
+	= { 0x0a, 0x04, 0x77, 0x69, 0x6c, 0x6c, 0x12, 0x04, 0x74, 0x65, 0x73, 0x74 };
 static gchar* ai_username = "will";
 static gchar* ai_password = "test";
+
+static const gsize ai_corrupted_encoded_len = 12;
+static const guint8 ai_corrupted_encoded[11] 
+	= { 0x0a, 0x04, 0x77, 0x69, 0x6c, 0x6c, 0x04, 0x74, 0x65, 0x73, 0x74 };
 
 static void test_packing_auth_proto(void) {
 	AuthInfo ai = AUTH_INFO__INIT;
@@ -22,7 +28,7 @@ static void test_packing_auth_proto(void) {
 
 	g_assert(len == ai_encoded_len);
 
-	for (gint i = 0; i < len; i++) {
+	for (gsize i = 0; i < len; i++) {
 		g_assert(buf[i] == ai_encoded[i]);
 	}
 
@@ -41,11 +47,24 @@ static void test_unpacking_auth_proto(void) {
 	auth_info__free_unpacked(ai, NULL);
 }
 
+static void test_unpacking_corrupted(void) {
+	AuthInfo* ai = NULL;
+
+	if (g_test_trap_fork(0, G_TEST_TRAP_SILENCE_STDOUT)) {
+		ai = auth_info__unpack(NULL, ai_corrupted_encoded_len, ai_corrupted_encoded);
+		exit(0);
+	}
+
+	g_test_trap_assert_stdout("unsupported tag * at offset *\n");
+	g_assert(ai == NULL);
+}
+
 int main(int argc, char** argv) {
 	g_test_init(&argc, &argv, NULL);
 
 	g_test_add_func("/Library/Protocol/PackAuthInfo", test_packing_auth_proto);
 	g_test_add_func("/Library/Protocol/UnpackAuthInfo", test_unpacking_auth_proto);
+	g_test_add_func("/Library/Protocol/UnpackCorruptedAuthInfo", test_unpacking_corrupted);
 
 	return g_test_run();
 }

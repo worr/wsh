b6cbb912aa90a446c085b11a45d2ca0f1242c343
Will Orr <will@worrbase.com>
2013-03-14 18:17:34 -0400

    Added support for easily parsing messages in wshd

--- server/src/parse.h
+++ server/src/parse.h
@@ -3,12 +3,15 @@
 
 #include <glib.h>
 
+#include "cmd.h"
+
 typedef union {
 	guint32 size;
 	gchar buf[4];
 } wshd_message_size_t;
 
 gint wshd_get_message_size(GIOChannel* std_output, GError* err);
+void wshd_get_message(GIOChannel* std_output, wsh_cmd_req_t** req, GError* err);
 
 #endif
 
--- server/test/test_parse.c
+++ server/test/test_parse.c
@@ -5,6 +5,25 @@
 #include "cmd.h"
 #include "parse.h"
 
+static gchar* req_username = "will";
+static gchar* req_password = "test";
+static gchar* req_cmd = "ls";
+static gchar* req_stdin[3] = { "yes", "no", NULL };
+static const gsize req_stdin_len = 2;
+static gchar* req_env[4] = { "PATH=/usr/bin", "USER=will", "MAILTO=will@worrbase.com", NULL };
+static const gsize req_env_len = 3;
+static gchar* req_cwd = "/tmp";
+static const guint64 req_timeout = 5;
+
+static const gsize encoded_req_len = 87;
+static const gchar encoded_req[87]
+	= { 0x0a, 0x02, 0x6c, 0x73, 0x12, 0x0c, 0x0a, 0x04, 0x77, 0x69, 0x6c, 0x6c, 0x12, 0x04, 0x74, 0x65, 0x73, 0x74, 
+	    0x1a, 0x03, 0x79, 0x65, 0x73, 0x1a, 0x02, 0x6e, 0x6f, 0x22, 0x0d, 0x50, 0x41, 0x54, 
+	    0x48, 0x3d, 0x2f, 0x75, 0x73, 0x72, 0x2f, 0x62, 0x69, 0x6e, 0x22, 0x09, 0x55, 0x53, 0x45, 0x52, 0x3d, 0x77, 
+		0x69, 0x6c, 0x6c, 0x22, 0x18, 0x4d, 0x41, 0x49, 0x4c, 0x54, 0x4f, 0x3d, 0x77, 0x69, 0x6c, 0x6c, 0x40, 0x77, 
+		0x6f, 0x72, 0x72, 0x62, 0x61, 0x73, 0x65, 0x2e, 0x63, 0x6f, 0x6d, 0x2a, 0x04, 0x2f, 0x74, 0x6d, 0x70, 0x30,
+		0x05 } ;
+
 static void test_get_message_size(void) {
 	wshd_message_size_t size;
 	gint recv;
@@ -34,10 +53,52 @@ static void test_get_message_size(void) {
 	g_assert(recv == ntohl(size.size));
 }
 
+static void test_get_message(void) {
+	wshd_message_size_t size;
+	wsh_cmd_req_t* req = g_new0(wsh_cmd_req_t, 1);
+	gint fds[2];
+	gsize writ;
+	GError* err = NULL;
+
+	pipe(fds);
+
+	GIOChannel* in = g_io_channel_unix_new(fds[1]);
+	GIOChannel* mock_stdout = g_io_channel_unix_new(fds[0]);
+	g_io_channel_set_encoding(in, NULL, NULL);
+	g_io_channel_set_encoding(mock_stdout, NULL, NULL);
+
+	size.size = htonl(encoded_req_len);
+	g_io_channel_write_chars(in, size.buf, 4, &writ, NULL);
+	g_io_channel_write_chars(in, encoded_req, encoded_req_len, &writ, NULL);
+	g_io_channel_flush(in, NULL);
+	wshd_get_message(mock_stdout, &req, err);
+
+	g_assert_no_error(err);
+	g_assert_cmpstr(req->cmd_string, ==, req_cmd);
+	g_assert_cmpstr(req->cwd, ==, req_cwd);
+	g_assert(req->timeout == req_timeout);
+	g_assert_cmpstr(req->username, ==, req_username);
+	g_assert_cmpstr(req->password, ==, req_password);
+	g_assert(req->std_input_len == req_stdin_len);
+
+	for (gsize i = 0; i < req_stdin_len; i++)
+		g_assert_cmpstr(req->std_input[i], ==, req_stdin[i]);
+
+	for (gsize i = 0; i < req_env_len; i++)
+		g_assert_cmpstr(req->env[i], ==, req_env[i]);
+
+	g_io_channel_shutdown(in, FALSE, NULL);
+	g_io_channel_shutdown(mock_stdout, FALSE, NULL);
+	close(fds[0]);
+	close(fds[1]);
+	g_free(req);
+}
+
 int main(int argc, char** argv) {
 	g_test_init(&argc, &argv, NULL);
 
 	g_test_add_func("/Server/Parser/GetSize", test_get_message_size);
+	g_test_add_func("/Server/Parser/GetMessage", test_get_message);
 
 	return g_test_run();
 }

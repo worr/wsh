436213b4938f688c0707e2acffd4fe64a7032637
William Orr <will@worrbase.com>
2013-07-15 09:22:02 -0700

    Ensure that ssh_authenticate fails gracefully

--- library/test/test_ssh.c
+++ library/test/test_ssh.c
@@ -248,6 +248,56 @@ static void authenticate_pubkey_successful(void) {
 	g_slice_free(wsh_ssh_session_t, session);
 }
 
+static void authenticate_kbdint_unsuccessful(void) {
+	set_ssh_connect_res(SSH_OK);
+	set_ssh_is_server_known_res(SSH_SERVER_KNOWN_OK);
+	set_ssh_userauth_list_ret(SSH_AUTH_METHOD_INTERACTIVE);
+	set_ssh_userauth_kbdint_ret(SSH_AUTH_ERROR);
+
+	wsh_ssh_session_t* session = g_slice_new0(wsh_ssh_session_t);
+	session->hostname = remote;
+	session->username = username;
+	session->password = password;
+	session->port = port;
+	session->auth_type = WSH_SSH_AUTH_PASSWORD;
+	GError *err = NULL;
+
+	wsh_ssh_host(session, &err);
+	gint ret = wsh_ssh_authenticate(session, &err);
+
+	g_assert(ret != 0);
+	g_assert(session->session == NULL);
+	g_assert_error(err, WSH_SSH_ERROR, 1);
+
+	g_error_free(err);
+	g_slice_free(wsh_ssh_session_t, session);
+}
+
+static void authenticate_kbdint_denied(void) {
+	set_ssh_connect_res(SSH_OK);
+	set_ssh_is_server_known_res(SSH_SERVER_KNOWN_OK);
+	set_ssh_userauth_list_ret(SSH_AUTH_METHOD_INTERACTIVE);
+	set_ssh_userauth_kbdint_ret(SSH_AUTH_DENIED);
+
+	wsh_ssh_session_t* session = g_slice_new0(wsh_ssh_session_t);
+	session->hostname = remote;
+	session->username = username;
+	session->password = password;
+	session->port = port;
+	session->auth_type = WSH_SSH_AUTH_PASSWORD;
+	GError *err = NULL;
+
+	wsh_ssh_host(session, &err);
+	gint ret = wsh_ssh_authenticate(session, &err);
+
+	g_assert(ret != 0);
+	g_assert(session->session == NULL);
+	g_assert_error(err, WSH_SSH_ERROR, 2);
+
+	g_error_free(err);
+	g_slice_free(wsh_ssh_session_t, session);
+}
+
 int main(int argc, char** argv) {
 	g_test_init(&argc, &argv, NULL);
 
@@ -269,6 +319,11 @@ int main(int argc, char** argv) {
 		authenticate_pubkey_denied);
 	g_test_add_func("/Library/SSH/AuthenticatePubkeySuccess",
 		authenticate_pubkey_successful);
+
+	g_test_add_func("/Library/SSH/AuthenticateKbdintUnsuccessfully",
+		authenticate_kbdint_unsuccessful);
+	g_test_add_func("/Library/SSH/AuthenticateKbdintFailure",
+		authenticate_kbdint_denied);
 
 	return g_test_run();
 }

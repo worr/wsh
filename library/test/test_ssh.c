7e7b92abeddb215424b0d97132e71c45880a35cd
William Orr <will@worrbase.com>
2013-07-14 19:55:37 -0700

    Ensure that wsh_ssh_authenticate bails gracefully on pubkey access denied

--- library/test/test_ssh.c
+++ library/test/test_ssh.c
@@ -198,6 +198,31 @@ static void authenticate_pubkey_unsuccessful(void) {
 	g_slice_free(wsh_ssh_session_t, session);
 }
 
+static void authenticate_pubkey_denied(void) {
+	set_ssh_connect_res(SSH_OK);
+	set_ssh_is_server_known_res(SSH_SERVER_KNOWN_OK);
+	set_ssh_userauth_list_ret(SSH_AUTH_METHOD_PUBLICKEY);
+	set_ssh_userauth_autopubkey(SSH_AUTH_DENIED);
+
+	wsh_ssh_session_t* session = g_slice_new0(wsh_ssh_session_t);
+	session->hostname = remote;
+	session->username = username;
+	session->password = password;
+	session->port = port;
+	session->auth_type = WSH_SSH_AUTH_PUBKEY;
+	GError *err = NULL;
+
+	wsh_ssh_host(session, &err);
+	gint ret = wsh_ssh_authenticate(session, &err);
+
+	g_assert(ret != 0);
+	g_assert(session->session == NULL);
+	g_assert_error(err, WSH_SSH_ERROR, 5);
+
+	g_error_free(err);
+	g_slice_free(wsh_ssh_session_t, session);
+}
+
 int main(int argc, char** argv) {
 	g_test_init(&argc, &argv, NULL);
 
@@ -215,6 +240,8 @@ int main(int argc, char** argv) {
 
 	g_test_add_func("/Library/SSH/AuthenticatePubkeyUnsuccessfully",
 		authenticate_pubkey_unsuccessful);
+	g_test_add_func("/Library/SSH/AuthenticatePubkeyFailure",
+		authenticate_pubkey_denied);
 
 	return g_test_run();
 }

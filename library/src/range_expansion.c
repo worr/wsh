21313f2ea09ee2b69d183b47e4995519b2ae525e
William Orr <will@worrbase.com>
2013-07-18 08:58:18 -0700

    Added range expansion files

---
+++ library/src/range_expansion.c
@@ -0,0 +1,63 @@
+#include "range_expansion.h"
+
+#include <apr_pools.h>
+#include <libcrange.h>
+
+const gsize MAX_APR_ERR_MSG_SIZE = 1024;
+
+gint wsh_exp_range_expand(gchar*** host_list, const gchar* string, GError** err) {
+	g_assert_no_error(*err);
+	g_assert(*host_list == NULL);
+	g_assert(string != NULL);
+
+	apr_initialize();
+
+	gint ret = 0;
+
+	apr_pool_t* pool;
+	apr_status_t stat = apr_pool_create(&pool, NULL);
+
+	WSH_EXP_ERROR = g_quark_from_static_string("wsh_exp_error");
+
+	if (stat != APR_SUCCESS) {
+		char* err_message = g_slice_alloc0(MAX_APR_ERR_MSG_SIZE);
+		apr_strerror(stat, err_message, MAX_APR_ERR_MSG_SIZE);
+		*err = g_error_new(WSH_EXP_ERROR, WSH_EXP_POOL_ALLOC_ERR,
+			err_message);
+		// I hope g_error_new makes a copy of that string!
+		g_slice_free1(MAX_APR_ERR_MSG_SIZE, err_message);
+		ret = WSH_EXP_POOL_ALLOC_ERR;
+		goto wsh_exp_range_expand_err;
+	}
+
+	libcrange* lr = libcrange_new(pool, NULL);
+	if (lr == NULL) {
+		*err = g_error_new(WSH_EXP_ERROR, WSH_EXP_RANGE_ALLOC_ERR,
+			"Could not allocate a libcrange object");
+		ret = WSH_EXP_RANGE_ALLOC_ERR;
+		goto wsh_exp_range_expand_err;
+	}
+
+	range_request* rr = range_expand(lr, pool, string);
+	if (range_request_has_warnings(rr)) {
+		*err = g_error_new(WSH_EXP_ERROR, WSH_EXP_RANGE_EXPANSION_ERR,
+			range_request_warnings(rr));
+		ret = WSH_EXP_RANGE_EXPANSION_ERR;
+		goto wsh_exp_range_expand_err;
+	}
+
+	*host_list = g_strdupv((gchar**)range_request_nodes(rr));
+	if (host_list == NULL) {
+		*err = g_error_new(WSH_EXP_ERROR, WSH_EXP_NO_HOSTS_ERR,
+			"%s expanded to 0 hosts", string);
+		ret = WSH_EXP_NO_HOSTS_ERR;
+		goto wsh_exp_range_expand_err;
+	}
+
+wsh_exp_range_expand_err:
+	apr_pool_clear(pool);
+	apr_pool_destroy(pool);
+
+	return ret;
+}
+
---
+++ library/src/range_expansion.h
@@ -0,0 +1,18 @@
+#ifndef __WSH_RANGE_EXPANSION_H
+#define __WSH_RANGE_EXPANSION_H
+
+#include <glib.h>
+
+GQuark WSH_EXP_ERROR;
+
+typedef enum {
+	WSH_EXP_POOL_ALLOC_ERR,
+	WSH_EXP_RANGE_EXPANSION_ERR,
+	WSH_EXP_NO_HOSTS_ERR,
+	WSH_EXP_RANGE_ALLOC_ERR,
+} wsh_exp_err_enum;
+
+gint wsh_exp_range_expand(gchar*** host_list, const gchar* string, GError** err);
+
+#endif
+

38348cd953290569320d59c928763698cfbd77d0
William Orr <will@worrbase.com>
2013-08-15 16:04:35 -0700

    Added memset_s compilation to wsh-askpass

---
+++ util/src/memset_s.c
@@ -0,0 +1,15 @@
+#include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int memset_s(void* v, size_t smax, int c, size_t n) {
+	if (v == NULL) return EINVAL;
+	if (smax > SIZE_MAX) return EINVAL;
+	if (n > smax) return EINVAL;
+
+	volatile unsigned char *p = v;
+	while (smax-- && n--) *p++ = c;
+
+	return 0;
+}
+

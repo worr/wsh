b5ace45d3b2bbed23dc072c4e94ede4a1d8fbdf5
William Orr <will@worrbase.com>
2013-08-29 17:04:28 -0700

    Add libwsh.h for other libs to use

---
+++ library/src/libwsh.h
@@ -0,0 +1,19 @@
+/** @file
+ * @brief Global libwsh header. Include this to get everything else
+ */
+#ifndef __LIBWSH_H
+#define __LIBWSH_H
+
+#include <libwsh/cmd.h>
+#include <libwsh/expansion.h>
+#include <libwsh/log.h>
+#include <libwsh/pack.h>
+#include <libwsh/ssh.h>
+#include <libwsh/types.h>
+
+#ifdef RANGE
+# include <libwsh/range_expansion.h>
+#endif
+
+#endif
+

0de9178b1015ed2b2c1702cfee9e1d3e5cd98da8
William Orr <will@worrbase.com>
2013-09-12 13:31:47 -0700

    Fixed run_cmd tests

--- library/test/test_run_cmd.c
+++ library/test/test_run_cmd.c
@@ -140,22 +140,22 @@ static void test_construct_sudo_cmd(struct test_wsh_run_cmd_data* fixture, gcons
 
 	req->sudo = TRUE;
 	res = wsh_construct_sudo_cmd(req);
-	g_assert_cmpstr(res, ==, "sudo -A -u root /bin/ls");
+	g_assert_cmpstr(res, ==, "sudo -sA -u root /bin/ls");
 	g_free(res);
 
 	req->username = "worr";
 	res = wsh_construct_sudo_cmd(req);
-	g_assert_cmpstr(res, ==, "sudo -A -u worr /bin/ls");
+	g_assert_cmpstr(res, ==, "sudo -sA -u worr /bin/ls");
 	g_free(res);
 
 	req->username = "";
 	res = wsh_construct_sudo_cmd(req);
-	g_assert_cmpstr(res, ==, "sudo -A -u root /bin/ls");
+	g_assert_cmpstr(res, ==, "sudo -sA -u root /bin/ls");
 	g_free(res);
 
 	req->username = " ";
 	res = wsh_construct_sudo_cmd(req);
-	g_assert_cmpstr(res, ==, "sudo -A -u root /bin/ls");
+	g_assert_cmpstr(res, ==, "sudo -sA -u root /bin/ls");
 	g_free(res);
 
 	req->cmd_string = "";

8bc7e80ae66c73fe4a2531ef68ef0a065e196a9a
William Orr <will@worrbase.com>
2013-12-02 10:58:45 -0800

    Fixed lib paths for LinkedIn's glib

--- cmake/Modules/LinkedIn.cmake
+++ cmake/Modules/LinkedIn.cmake
@@ -1,2 +1,15 @@
-set( CMAKE_LIBRARY_PATH /usr/local/lib/wsh/lib )
-set( CMAKE_INCLUDE_PATH /usr/local/lib/wsh/include )
+set( CMAKE_LIBRARY_PATH
+	/usr/lib64/wsh/lib64
+	/usr/lib/wsh/lib
+	/usr/pkg/lib/wsh/lib
+	/usr/local/lib/wsh/lib
+	/usr/local/lib64/wsh/lib64
+)
+
+set( CMAKE_INCLUDE_PATH
+	/usr/lib64/wsh/include
+	/usr/lib/wsh/include
+	/usr/pkg/lib/wsh/include
+	/usr/local/lib/wsh/include
+	/usr/local/lib64/wsh/include
+)

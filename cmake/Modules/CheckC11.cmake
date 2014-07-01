0dd970c67ce351b4c2ba4d1d2919d4b326009d29
William Orr <will@worrbase.com>
2014-07-01 16:14:53 -0700

    Define libc extensions if c11 is set

--- cmake/Modules/CheckC11.cmake
+++ cmake/Modules/CheckC11.cmake
@@ -4,4 +4,8 @@ macro( check_c11 )
 	set( CMAKE_REQUIRED_FLAGS "-std=c11" )
 	check_c_source_compiles( "int main(void) { return 0; }" HAVE_C11 )
 	set( CMAKE_REQUIRED_FLAGS "" )
+
+	if( HAVE_C11 )
+		add_definitions( -D__STDC_WANT_LIB_EXT1__ )
+	endif( HAVE_C11 )
 endmacro( check_c11 )

be2ec1a389265b3276cb5dd8936082fc07e94333
William Orr <will@worrbase.com>
2014-07-08 23:16:16 -0700

    Show unit and correct timeout default in wshc --help. Fixes #85

--- client/src/main.c
+++ client/src/main.c
@@ -46,7 +46,7 @@ static gchar* username = NULL;
 static gboolean ask_password = FALSE;
 static gchar* sudo_username = NULL;
 static gint threads = 0;
-static gint timeout = -1;
+static gint timeout = 300;
 static gchar* script = NULL;
 static gboolean version = FALSE;
 
@@ -67,7 +67,7 @@ static GOptionEntry entries[] = {
 	{ "password", 'p', 0, G_OPTION_ARG_NONE, &ask_password, "Prompt for SSH password", NULL },
 	{ "sudo-username", 'U', 0, G_OPTION_ARG_STRING, &sudo_username, "sudo username", NULL },
 	{ "threads", 't', 0, G_OPTION_ARG_INT, &threads, "Number of threads to use (default: 0)", NULL },
-	{ "timeout", 'T', 0, G_OPTION_ARG_INT, &timeout, "Timeout before killing command (default: 30)", NULL },
+	{ "timeout", 'T', 0, G_OPTION_ARG_INT, &timeout, "Timeout before killing command (default: 300 seconds)", NULL },
 	{ "script", 's', 0, G_OPTION_ARG_FILENAME, &script, "Script to execute on remote host", NULL },
 	{ "version", 'V', 0, G_OPTION_ARG_NONE, &version, "Print the version number", NULL },
 
@@ -99,7 +99,7 @@ static void build_wsh_cmd_req(wsh_cmd_req_t* req, gchar* password, gchar* cmd) {
 	req->env = NULL;
 	req->std_input = NULL;
 	req->std_input_len = 0;
-	req->timeout = timeout == -1 ? 300 : timeout;
+	req->timeout = timeout;
 	req->cwd = "";
 	req->host = g_strdup(g_get_host_name());
 	req->cmd_string = cmd;

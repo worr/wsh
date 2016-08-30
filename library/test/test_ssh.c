d33c6692da9365541b8b1077083d4d6d7d2ce2fe
Pradeep Sanders <psanders@psanders-mn2.linkedin.biz>
2016-08-30 16:18:31 -0700

    Set use_shell to optional, for backward compatibility

--- library/test/test_ssh.c
+++ library/test/test_ssh.c
@@ -516,7 +516,6 @@ static void send_cmd_write_success(void) {
 	req->timeout = req_timeout;
 	req->username = req_username;
 	req->password = req_password;
-	req->use_shell = TRUE;
 
 	GError* err = NULL;
 
@@ -524,7 +523,7 @@ static void send_cmd_write_success(void) {
 	wsh_ssh_exec_wshd(session, &err);
 
 	reset_ssh_channel_write_first(TRUE);
-	set_ssh_channel_write_ret(91);
+	set_ssh_channel_write_ret(89);
 	gint ret = wsh_ssh_send_cmd(session, req, &err);
 
 	g_assert(ret == 0);

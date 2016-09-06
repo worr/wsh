6a78f225f0f8205eba0deaf074bff8035d52a14b
William Orr <will@worrbase.com>
2016-09-06 00:18:20 -0700

    debugging of travis

--- coveralls.sh
+++ coveralls.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+set -x
+
 if [ "$CFLAGS" = "" ] && [ "$CC" = "gcc" ]; then
+   pwd
    pip install --user cpp-coveralls
    coveralls -y .coveralls.yml -r .. --exclude server/test --exclude library/test --exclude util --exclude client/test --exclude build/CMakeFiles --exclude build/gen --gcov-options '\-lp'
 fi

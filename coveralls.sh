73841c6c477cd94ff753e2eebbb780c0bc6c092f
William Orr <will@worrbase.com>
2016-09-06 00:04:35 -0700

    Twiddle excludes in coveralls script

--- coveralls.sh
+++ coveralls.sh
@@ -2,5 +2,5 @@
 
 if [ "$CFLAGS" = "" ] && [ "$CC" = "gcc" ]; then
    pip install --user cpp-coveralls
-   coveralls --exclude server/test --exclude library/test --exclude util --exclude client/test --exclude build --gcov-options '\-lp'
+   coveralls -y .coveralls.yml -r .. --exclude server/test --exclude library/test --exclude util --exclude client/test --exclude build/CMakeFiles --exclude build/gen --gcov-options '\-lp'
 fi

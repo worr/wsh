ab588ffee57f5ff1d8d0a113615b8649877061c8
Pradeep Sanders <psanders@linkedin.com>
2018-10-17 10:20:38 -0700

    Remove libssh_threads library as it's now part of the main lib

--- cmake/Modules/FindSSH.cmake
+++ cmake/Modules/FindSSH.cmake
@@ -11,12 +11,7 @@ find_library( LIBSSH_LIBRARY
 
 find_path( LIBSSH_INCLUDE_DIR libssh/libssh.h )
 
-find_library( LIBSSH_THREADS_LIBRARY
-		NAMES ssh_threads
-		PATHS /usr/local/lib /usr/local/lib64 /usr/pkg/lib /opt/lib /usr/lib64 /usr/lib /lib /lib64
-)
-
 include( FindPackageHandleStandardArgs )
-find_package_handle_standard_args( LibSSH DEFAULT_MSG LIBSSH_INCLUDE_DIR LIBSSH_LIBRARY LIBSSH_THREADS_LIBRARY )
+find_package_handle_standard_args( LibSSH DEFAULT_MSG LIBSSH_INCLUDE_DIR LIBSSH_LIBRARY )
 
-mark_as_advanced( LIBSSH_INCLUDE_DIR LIBSSH_LIBRARY LIBSSH_THREADS_LIBRARY )
+mark_as_advanced( LIBSSH_INCLUDE_DIR LIBSSH_LIBRARY )
